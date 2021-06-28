9f3fe47b707924cd3a6114d0f76af0654d431203
Rolando Aguilera <rolandoaguilera90@gmail.com>
2021-06-29 00:03:53 +0800

    Fix project error and live editor at startup

--- Data_Scripts/setup_project.m
+++ Data_Scripts/setup_project.m
@@ -6,8 +6,8 @@ cfg = Simulink.fileGenControl('getConfig');
 cfg.CacheFolder = [proj.RootFolder, '\work'];
 cfg.CodeGenFolder = [proj.RootFolder, '\src'];
 Simulink.fileGenControl('setConfig', 'config', cfg);
-builddocsearchdb(fullfile(proj.RootFolder,'Doc','HTML'))
-web(fullfile(proj.RootFolder,'Doc','HTML'),'BMS.html')
+%builddocsearchdb(fullfile(proj.RootFolder,'Doc','HTML'))
+%web(fullfile(proj.RootFolder,'Doc','HTML'),'BMS.html')
 cd([proj.RootFolder]);
 load('BattCrntDrvCycle');
 clear cfg;
---
+++ openLiveEditor.m
@@ -0,0 +1,1 @@
+edit BMSLiveEditor.mlx
