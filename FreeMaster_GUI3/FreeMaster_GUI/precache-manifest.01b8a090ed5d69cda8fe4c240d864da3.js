be31312584e88d60ccbc8f7c2590736b1e31a622
RolandoA <rolandoaguilera90@gmail.com>
2021-07-23 17:36:42 +0800

    Able to connect to freeMasters

---
+++ FreeMaster_GUI3/FreeMaster_GUI/precache-manifest.01b8a090ed5d69cda8fe4c240d864da3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e059e92b1ea999ace97eae6b2e36074",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27f3198a7ceb2f241582",
+    "url": "./static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "afc39a5868e19aec80fa",
+    "url": "./static/css/main.4b98f662.chunk.css"
+  },
+  {
+    "revision": "27f3198a7ceb2f241582",
+    "url": "./static/js/2.dda5ba3a.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "./static/js/2.dda5ba3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc39a5868e19aec80fa",
+    "url": "./static/js/main.d68d63c0.chunk.js"
+  },
+  {
+    "revision": "7a833fefead15dc83aa8",
+    "url": "./static/js/runtime-main.d6a7b5d6.js"
+  }
+]);
